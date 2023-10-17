0681bbfd392d00be171e882907a2c1e1db6b16a4
Akshat Gupta <guptaakshat370@gmai.com>
2023-10-17 19:51:28 +0530

    Food Ordering App Full Commit

---
+++ unpack.bat
@@ -0,0 +1,15 @@
+md assets
+move 1-products.js assets
+move 2-cart.css assets
+move 2-cart.js assets
+move 3-manager.css assets
+move 3-manager.js assets
+move 5-back.json assets
+move 5-front.json assets
+move 5-worker.js assets
+curl https://user-images.githubusercontent.com/11156244/275534573-560c7a1e-0b2d-4e9f-bfbf-43bd92c720c8.png --ssl-no-revoke --output assets/burger.png
+curl https://user-images.githubusercontent.com/11156244/275534597-075d731d-2ea8-410d-8b85-a0c2daecd990.png --ssl-no-revoke --output assets/pizza.png
+curl https://user-images.githubusercontent.com/11156244/244247124-eeaa9dfb-b845-40cc-a8e8-5dde467f1675.png --ssl-no-revoke --output assets/favico.png
+curl https://user-images.githubusercontent.com/11156244/244247136-48360ca4-a8c8-42b8-b131-401a22913831.png --ssl-no-revoke --output assets/ico-512.png
+call npm i express peer
+node 4-server.js
